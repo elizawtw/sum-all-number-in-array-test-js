47f2d42ce2c1014da7a5971a6f220e6d03e5a4ed
elizawtw <twew02@gmail.com>
2021-06-05 19:17:39 +0000

    sum all numbers in array

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
+
+const sumItems = function(array) {
+  let result = 0;
+  if (typeof array === 'number') {
+    return array;
+    
+  } else if (array instanceof Array) {
+    array.forEach((elem) => {
+      result += sumItems(elem);
+    });
+    
+  }return result;
 }
 
 module.exports = sumItems;
